2591dc40cce76e34ebf287b3d517ead79f560058
Cory Lueninghoener <cluening@gmail.com>
2015-01-12 22:26:04 -0700

    Added a tiny postscript header

--- gpx2ps.py
+++ gpx2ps.py
@@ -148,6 +148,7 @@ def main():
   # Start printing out the postscript
   #
 
+  print "%!PS"
   print "%% Generated with %s" % commandline
   print "90 rotate"
   print "%d %d translate" % (0, papersize[0]*-1)
